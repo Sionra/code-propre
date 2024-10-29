46438a23bfe778df951952284f15d64dce9e23ad
Sionra <71220351+Sionra@users.noreply.github.com>
2024-10-29 14:40:21 +0100

    EX 4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -6,14 +6,12 @@ import java.util.Date;
 public class DateUtils {
 
 	public static String format(String pattern, Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
-		return formateur.format(date);
-	}
-	
-	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat formateur;
+		if (pattern == null | pattern == ""){
+			formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		} else {
+			formateur = new SimpleDateFormat(pattern);
+		}
 		return formateur.format(date);
 	}
 }
